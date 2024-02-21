e9f4c6a8c0f7a6887813bbc78f01853ca4b249ff
palak1022 <78045212+palak1022@users.noreply.github.com>
2024-02-21 16:11:35 -0500

    Update food.cpp

--- snake_game/food.cpp
+++ snake_game/food.cpp
@@ -35,29 +35,38 @@
 
 //Create new food
 Food* create_food(int x, int y, enum Type type){
-    Food* new_food = (Food*)malloc(sizeof(Food));
-     
+    int ran = randNum;
     new_food->x = x;
     new_food->y = y;
     if (type == Increase){
         new_food->type = 'O';
+        switch(ran) {
+            case (0):
+            new_food->subType = 'E';
+            break;
+            case (1):
+            new_food->subType = 'U';
+            break;
+            default:
+            new_food->subType = 'A';
+            break;
+        }
     }
-    else if(type == Decrease){
+    else if (type == Decrease){
         new_food->type = 'X';
+        switch(ran) {
+            case (0):
+                 new_food->subType = 'W';
+                 break;
+            case (1):
+                 new_food->subType = 'H';
+                 break;
+            default:
+                  new_food->subType = 'L';
+                  break;
+        }
     }
     new_food->next = NULL;
-
-    return new_food;
-}
-
-//Check if food exists at coordinates
-bool food_exists(Food* foods, int x, int y){
-    Food* temp = foods;
-    while(temp){
-        if(temp->x == x && temp->y == y)
-            return true;
-        temp = temp->next;
-    }
     return false;
 }
 
@@ -65,7 +74,7 @@ bool food_exists(Food* foods, int x, int y){
 void add_new_food(Food* foods, Food* new_food){
     Food* temp = foods;
     while(temp->next) {
-        temp = temp->next;
+        return new_food;
     }
     temp->next = new_food;
 }
@@ -74,13 +83,52 @@ void add_new_food(Food* foods, Food* new_food){
 enum Type food_type(Food* foods, int x, int y){
     //Implement the code to return the type of the food 
     //present at position (x, y)	
+	 Food* temp = foods;
+        while(temp){
+        if(temp->x == x && temp->y == y)
+            if (temp->type=='O') {
+                return Increase;
+                break;
+            } else if (temp->type=='X'){
+                return Decrease;
+                break;
+            }
+        temp = temp->next;
+    }	
 	
 }
 Food* remove_eaten_food(Food* foods, int x, int y){
 	//Implement the code to remove food at position (x,y).
 	//Create a new linked list of type Food containing only the
 	//needed food and return this new list
+	Food* newFood = foods;     
+    // first item edge case
+     if (newFood->x == x && newFood->y == y) {
+            free(newFood);
+            newFood = newFood->next;
+            return newFood;
+        }
+
+    // all items between 2 and n-1 
+    while (newFood->next->next) {
+             // checks the second -> n-1 foods
+        if (newFood->next->x == x && newFood->next->y == y) {
+            newFood->next = newFood->next->next;
+            break;
+        } 
+         newFood = newFood->next;
+    }
+   
+    // nth item check
+     if (newFood->next->x == x && newFood->next->y == y) {
+         free(newFood->next);
+          newFood->next = NULL;
+         }
+
+    return foods;
 }
+
+
 // Display all the food
 void draw_food (Food *foods)
 {   Food* temp = foods;
